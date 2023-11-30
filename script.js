1dd2180ebdc4dc96694e1e8f3713cfe57dbc9da4
RenatSouz <rcsouza1987@gmail.com>
2023-11-30 15:09:06 -0300

    comentarios inseridos

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
+//retorna o elemento digitado
 document.getElementById('contactForm').addEventListener('submit', function(event) {
+ //prevenir o comportamento padrão de um evento
   event.preventDefault();
-
-  const nome = document.getElementById('nome').value;
+   const nome = document.getElementById('nome').value;
   const email = document.getElementById('email').value;
   const mensagem = document.getElementById('mensagem').value;
   const status = document.getElementById('status');
-
+  //tratamento das ações se o campo ficar em branco  
   if (nome.trim() === '' || email.trim() === '' || mensagem.trim() === '') {
     status.textContent = 'Por favor, preencha todos os campos.';
     status.style.color = 'red';
   } else {
+  //se estiver tudo ok
     status.textContent = 'Mensagem enviada!';
     status.style.color = 'green';
     this.reset();
